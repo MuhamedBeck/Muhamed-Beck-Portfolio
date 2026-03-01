cb0dd689b861c7568f6cbbbbf3436138b45a7033
Muhamed Nur Beck <m.nur.beck@hotmail.de>
2026-03-01 02:18:31 +0100

    make logo bigger

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -70,7 +70,7 @@ export const LoadingScreen = ({ onComplete }) => {
       <div className="relative z-10 text-center max-w-4xl mx-auto px-4 w-full">
         <div className="mb-8">
           <div className="flex justify-center mb-8">
-            <img src={mnbLogo} alt="MNB Logo" className="h-16 w-auto opacity-90" />
+            <img src={mnbLogo} alt="MNB Logo" className="h-28 w-auto opacity-90" />
           </div>
           <h1 className="text-4xl sm:text-6xl md:text-8xl font-light tracking-wider mb-4 whitespace-nowrap">
             <span className="bg-gradient-to-r from-blue-400 via-cyan-400 to-indigo-400 bg-clip-text text-transparent">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="flex items-center">
-            <img src={mnbLogo} alt="MNB Logo" className="h-9 w-auto" />
+            <img src={mnbLogo} alt="MNB Logo" className="h-12 w-auto" />
           </Link>
 
           <div
